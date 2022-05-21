83c3ed59e3cc27212b4be12e24dca1baf4603a77
conversaShawn <shawn.harris3510@gmail.com>
2022-05-20 20:25:55 -0700

    points onclick working

--- src/components/points/MapPoints.js
+++ src/components/points/MapPoints.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default function MapPoints() {
-    const points = [100, 200, 300, 400, 500]
-    return ( 
-        <div>
-            {points.map(p => <div>{p}</div>)}
-        </div>
-    );
+  const points = [100, 200, 300, 400, 500];
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("You clicked submit.");
+  }
+  return (
+    <div onClick={handleSubmit}>
+      {points.map((p) => (
+        <div type="submit">{p}</div>
+      ))}
+    </div>
+  );
 }
